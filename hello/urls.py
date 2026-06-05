c069c14788f90e962f29908d6d127beedf500e14
ThinkBotX <alianasahmed2024@gmail.com>
2026-06-05 18:08:56 +0600

    AGAIN!!

--- hello/urls.py
+++ hello/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("anas", views.anas, name="anas"),
     path("ali", views.ali, name="ali")
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def anas(request):
-    return HttpResponse("Hello, Anas!")
+    return render(request, "hello/anas.html")
 
 def ali(request):
-    return HttpResponse("Hello, Ali!")
+    return render(request, "hello/ali.html")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
