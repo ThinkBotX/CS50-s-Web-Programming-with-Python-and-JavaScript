fc26f530c4d82417beda2d54ad944ee275c799c3
ThinkBotX <alianasahmed2024@gmail.com>
2026-06-05 18:25:30 +0600

    NOt AGAIn !!!

---
+++ newyear/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    }
+)
