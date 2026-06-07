11ea88e0de181e94c78e1f2d558c644e6bafabb4
ThinkBotX <alianasahmed2024@gmail.com>
2026-06-07 19:56:42 +0600

    Once again

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for lecture3 project.
+URL configuration for airline project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -19,8 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls")),
-    path('tasks/', include("tasks.urls")),
-    path('newyear/', include("newyear.urls"))
+    path('flights/', include('flights.urls')),
 ]
-
